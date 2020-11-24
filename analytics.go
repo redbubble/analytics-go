0dec7d4fef64c4a8f6c90d4fa27c74daa45d33e8
ivan.van <ivan.van.melb.93@gmail.com>
2020-11-24 15:52:09 +1100

    retrying

--- analytics.go
+++ analytics.go
@@ -271,6 +271,8 @@ func (c *client) send(msgs []message) {
 		// Wait for either a retry timeout or the client to be closed.
 		select {
 		case <-time.After(c.RetryAfter(i)):
+			fmt.Println("gonna retry")
+			break
 		case <-c.quit:
 			//c.errorf("%d messages dropped because they failed to be sent and the client was closed", len(msgs))
 			//c.notifyFailure(msgs, err)
